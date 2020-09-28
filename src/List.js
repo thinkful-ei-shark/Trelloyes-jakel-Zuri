849d018072cd216800d7405110d4e3dcc50a0435
JaKel Jenkins <jakelj24@gmail.com>
2020-09-28 16:32:20 -0400

    List updated

---
+++ src/List.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Card from './Card';
+
+const card = 
+function List(props) {
+  return (
+    <div className='List'>
+      {/* content goes here */}
+      <section class="List">
+        <header class="List-header">
+          <h2>First list</h2>
+        </header>
+        <div class="List-cards">
+            
+          </div>
+          </section>
+    </div>
+  );
+}
+
+
+
+
+
+
+
+
